854f0bbd177921264b1cdf6bedeec856fd70f8c4
vadimhl <vadimhl@gmail.com>
2024-11-02 19:55:54 +0200

    feat: main guess loop

--- number_guess.sh
+++ number_guess.sh
@@ -24,6 +24,14 @@ else
   SECRET=$(( RANDOM % 1000 + 1 ))
   echo "SECRET=$SECRET"
   echo Guess the secret number between 1 and 1000:
-  
+  read GUESS
+  GUESSES=1
+  while [[ $GUESS != $SECRET ]]
+  do
+    echo "It's lower than that, guess again:"
+    read GUESS
+    ((GUESSES++))
+  done
+  echo "You guessed it in $GUESSES tries. The secret number was $SECRET. Nice job!"
   
 fi
